6f000d2fbe96a73751d310a59ad2fc10f4dca7e8
Arjay Onan <archessonan.1@gmail.com>
2023-08-29 10:25:08 +0800

    edited root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    root 'tokens#create'
+    root 'admins#create'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
