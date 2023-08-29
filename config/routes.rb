feda512941779dfe1f4fffced320e62afce3088d
Arjay Onan <archessonan.1@gmail.com>
2023-08-29 09:44:17 +0800

    edited route url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'admins/create'
-  end
+  root 'rails#server'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
