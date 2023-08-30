0b13e4d856b1e08a31f367e98c897004ec68951f
Arjay Onan <archessonan.1@gmail.com>
2023-08-30 14:39:18 +0800

    edited buy and sell function

--- lib/stock_computing_api.rb
+++ lib/stock_computing_api.rb
@@ -11,11 +11,11 @@ class StockComputingApi
     trader.update_attribute(:balance, trader.balance + profit.to_f)
   end
 
-  def self.sell_profit(ticker)
-    ticker.share * (ticker.last_price - ticker.latest_price)
-  end
-
-  def self.compute_percent(ticker)
-    format('%.2f', ((ticker.latest_price.to_f - ticker.last_price.to_f) / ticker.last_price.to_f) * 100)
+  def self.sell_profit(ticker, transaction, trader)
+    profit = format('%.2f', ticker.share * (ticker.last_price - ticker.latest_price).to_f)
+    percent = format('%.2f', (ticker.latest_price.to_f - ticker.last_price.to_f) / ticker.last_price.to_f)
+    transaction.update_attribute(:profit, profit.to_f)
+    transaction.update_attribute(:percent, percent.to_f)
+    trader.update_attribute(:balance, trader.balance + profit.to_f)
   end
 end
